f22327e1855c97999fb989172ed9242e3ab3127a
Shinyoung Yi <104247324+youngyii@users.noreply.github.com>
2025-11-17 23:14:29 +0900

    feat: add BaseEntity as common JPA superclass (#26)

---
+++ src/main/java/com/kt/common/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.kt.common;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+// 공통 베이스 엔티티
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    protected LocalDateTime createdAt;
+    protected LocalDateTime updatedAt;
+}
