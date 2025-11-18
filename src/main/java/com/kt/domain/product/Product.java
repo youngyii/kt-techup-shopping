717fef18a2ebacadb4157241ef2b86e0c330d305
Shinyoung Yi <104247324+youngyii@users.noreply.github.com>
2025-11-18 10:31:44 +0900

    feat: add Product entity and ProductStatus enum (#29)

---
+++ src/main/java/com/kt/domain/product/Product.java
@@ -0,0 +1,23 @@
+package com.kt.domain.product;
+
+import com.kt.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+@Entity
+public class Product extends BaseEntity {
+    private String name;
+    private Long price;
+    private Long stock;
+    @Enumerated(EnumType.STRING)
+    private ProductStatus status;
+
+    // TODO: 생성
+    // TODO: 수정
+    // TODO: 삭제
+    // TODO: 조회 (리스트, 단건)
+    // TODO: 상태 변경
+    // TODO: 재고 수량 감소
+    // TODO: 재고 수량 증가
+}
