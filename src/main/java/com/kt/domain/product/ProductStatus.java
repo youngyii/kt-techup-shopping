cd5374cd63fac539c8c7eb2a68cc4d9de273e27f
Shinyoung Yi <yishinyoung0708@gmail.com>
2025-11-20 22:01:07 +0900

    feat: add deleted status and rename ProductStatus values

--- src/main/java/com/kt/domain/product/ProductStatus.java
+++ src/main/java/com/kt/domain/product/ProductStatus.java
@@ -6,9 +6,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ProductStatus {
-    ACTIVE("판매중"),
+    ACTIVATED("판매중"), // 상품의 초기 상태
     SOLD_OUT("품절"),
-    INACTIVE("판매중지");
+    IN_ACTIVATED("판매중지"),
+    DELETED("삭제");
 
     private final String description;
 }
