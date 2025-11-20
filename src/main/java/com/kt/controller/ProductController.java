8bdecb86c070d44b4da745b70320707851392d84
Shinyoung Yi <yishinyoung0708@gmail.com>
2025-11-20 23:24:21 +0900

    feat: add ProductController

---
+++ src/main/java/com/kt/controller/ProductController.java
@@ -0,0 +1,72 @@
+package com.kt.controller;
+
+import com.kt.common.ApiResult;
+import com.kt.common.SwaggerAssistance;
+import com.kt.domain.product.ProductRequest;
+import com.kt.service.ProductService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "Product")
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController extends SwaggerAssistance {
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResult<Void> create(@RequestBody @Valid ProductRequest.Create request) {
+        productService.create(
+                request.getName(),
+                request.getPrice(),
+                request.getQuantity()
+        );
+
+        return ApiResult.ok();
+    }
+
+    @PutMapping("/{id}")
+    public ApiResult<Void> update(
+            @PathVariable Long id,
+            @RequestBody @Valid ProductRequest.Update request
+    ) {
+        productService.update(
+                id,
+                request.getName(),
+                request.getPrice(),
+                request.getQuantity()
+        );
+
+        return ApiResult.ok();
+    }
+
+    @PatchMapping("/{id}/sold-out")
+    public void soldOut(@PathVariable Long id) {
+        productService.soldOut(id);
+    }
+
+    @PatchMapping("/{id}/activate")
+    public ApiResult<Void> activate(@PathVariable Long id) {
+        productService.activate(id);
+
+        return ApiResult.ok();
+    }
+
+    @PatchMapping("/{id}/in-activate")
+    public ApiResult<Void> inActivate(@PathVariable Long id) {
+        productService.inActivate(id);
+
+        return ApiResult.ok();
+    }
+
+    @DeleteMapping("/{id}")
+    public ApiResult<Void> remove(@PathVariable Long id) {
+        productService.delete(id);
+
+        return ApiResult.ok();
+    }
+}
