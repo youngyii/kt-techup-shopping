ad7f478e4e3086481418f258c78dc7eb2c169136
Shinyoung Yi <yishinyoung0708@gmail.com>
2025-11-21 01:42:13 +0900

    refactor: consolidate user request DTOs into UserRequest

--- src/main/java/com/kt/dto/user/UserRequest.java
+++ src/main/java/com/kt/dto/user/UserRequest.java
@@ -5,8 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 import java.time.LocalDate;
 
