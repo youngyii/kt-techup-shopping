eff3086aa77e938e1bb4c6d4809177fc30dedbd0
Shinyoung Yi <104247324+youngyii@users.noreply.github.com>
2025-11-20 22:48:07 +0900

    feat: add base Product domain including entity, status, request DTOs and repository (#34)
    * feat: add deleted status and rename ProductStatus values
    * feat: add basic CRUD operations in Product entity
    * feat: add ProductRequest DTO for product operations
    * feat: add ProductRepository with custom find method

--- src/main/java/com/kt/domain/product/Product.java
+++ src/main/java/com/kt/domain/product/Product.java
@@ -6,26 +6,59 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Product extends BaseEntity {
     private String name;
     private Long price;
     private Long stock;
     @Enumerated(EnumType.STRING)
-    private ProductStatus status;
-
+    private ProductStatus status = ProductStatus.ACTIVATED;
     @OneToMany(mappedBy = "product")
     private List<OrderProduct> orderProducts = new ArrayList<>();
 
-    // TODO: 생성
-    // TODO: 수정
-    // TODO: 삭제
+    public Product(String name, Long price, Long stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public void update(String name, Long price, Long stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public void soldOut() {
+        this.status = ProductStatus.SOLD_OUT;
+    }
+
+    public void inActivate() {
+        this.status = ProductStatus.IN_ACTIVATED;
+    }
+
+    public void activate() {
+        this.status = ProductStatus.ACTIVATED;
+    }
+
+    public void delete() {
+        this.status = ProductStatus.DELETED;
+    }
+
+    public void decreaseStock(Long quantity) {
+        this.stock -= quantity;
+    }
+
+    public void increaseStock(Long quantity) {
+        this.stock += quantity;
+    }
+
     // TODO: 조회 (리스트, 단건)
-    // TODO: 상태 변경
-    // TODO: 재고 수량 감소
-    // TODO: 재고 수량 증가
 }
